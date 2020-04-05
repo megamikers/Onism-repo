cfdb8a1b42a46734cf47dd343cedae6de1b0afff
Michael Mederos <megamikers@yahoo.com>
2020-04-04 22:10:44 -0400

    Fixed Collectable again

--- Assets/Scripts/Obstacle Scripts/RotatingCollectables.cs
+++ Assets/Scripts/Obstacle Scripts/RotatingCollectables.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectables : MonoBehaviour
+public class RotatingCollectables : MonoBehaviour
 {
     private void Update()
     {
