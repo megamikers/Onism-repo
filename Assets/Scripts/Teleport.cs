aec2bdc788005a83158077b58c3832ecd110b834
Michael Mederos <megamikers@yahoo.com>
2020-04-04 22:42:17 -0400

    Added Teleporter and Downsized Game
    The teleport script and various teleporters have been added to the game. Teleporter Prefab added simply Drag the player to the player area and the transform of the desired position for teleporter. Downsize of game from 6 section to 4 due to COVID-19 pendemic

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public Transform teleportTarget; //teleport position
+	public GameObject thePlayer; //teleporting player
+
+	void OnTriggerEnter(Collider other)
+	{
+		thePlayer.transform.position = teleportTarget.transform.position;
+		//Teleports the player to the target position
+	}
+}
