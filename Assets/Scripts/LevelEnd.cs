b5251a237e6bbf75431a6247c0cdfa0cd95039e6
Michael Mederos <megamikers@yahoo.com>
2020-04-18 16:14:54 -0400

    Added Level ending Teleporter
    Added teleporter at the end of the level to send us to the Credit scene

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		SceneManager.LoadScene("Credit");
+	}
+}
